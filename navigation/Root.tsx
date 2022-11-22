692b58099835457c7101fa5dcf12010b83769c19
Sangmin Lee <gink.ss00@gmail.com>
2022-11-23 04:24:50 +0900

    update: 헤더 부분 삭제로 인한 변경
    헤더 부분에 구현해놓았던 것들을 삭제한다. 따로 헤더 부분이 아닌 위쪽에 구현할 예정이다.
    또한, headerShown을 각각의 스크린에 구현해놓았는데 이를 한번에 navigator에 구현하였다.

--- navigation/Root.tsx
+++ navigation/Root.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Stack from './Stack';
-import { Octicons } from '@expo/vector-icons';
-import styled from 'styled-components/native';
 import Login from '../screens/Login/Login';
 import DetectPestResult from '../screens/DetectPestResult';
 import Drawer from './Drawer';
 
-const HeaderLeftLogo = styled.Text`
-  font-size: 30px;
-  font-weight: 600;
-  color: #48a346;
-`;
-const HeaderRightBtnsContainer = styled.View`
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-`;
-const HeaderRightBtn = styled.TouchableOpacity`
-  margin-left: 18px;
-`;
 const Nav = createNativeStackNavigator();
 
 const Root = ({ jwtToken }) => {
@@ -29,26 +14,14 @@ const Root = ({ jwtToken }) => {
         contentStyle: {
           backgroundColor: '#F7FBF9',
         },
-        headerBackTitleVisible: false,
-        headerStyle: {
-          backgroundColor: '#F7FBF9',
-        },
-        headerTitleStyle: {
-          color: 'white',
-        },
+        headerShown: false,
       }}
     >
-      {!jwtToken ? (
-        <Nav.Screen name="Login" component={Login} options={{ headerShown: false }} />
-      ) : null}
-      <Nav.Screen name="Drawer" component={Drawer} options={{ headerShown: false }} />
-      <Nav.Screen name="Stack" component={Stack} options={{ headerShown: false }} />
-      <Nav.Screen
-        name="DetectPestResult"
-        component={DetectPestResult}
-        options={{ headerShown: false }}
-      />
-      <Nav.Screen name="SameLogin" component={Login} options={{ headerShown: false }} />
+      {!jwtToken ? <Nav.Screen name="Login" component={Login} /> : null}
+      <Nav.Screen name="Drawer" component={Drawer} />
+      <Nav.Screen name="Stack" component={Stack} />
+      <Nav.Screen name="DetectPestResult" component={DetectPestResult} />
+      <Nav.Screen name="SameLogin" component={Login} />
     </Nav.Navigator>
   );
 };
