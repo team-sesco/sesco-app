adc85a77db120a614d57cfb66933cb2fbb8772ae
Sangmin Lee <gink.ss00@gmail.com>
2022-11-23 03:54:02 +0900

    feat: Drawer 수정 및 스타일 변경

--- navigation/Drawer.tsx
+++ navigation/Drawer.tsx
@@ -1,18 +1,48 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerItemList,
+} from '@react-navigation/drawer';
 import ChangeName from '../screens/ChangeName';
 import Main from '../screens/Main';
+import PersonalInfoPolicy from '../screens/PersonalInfoPolicy';
 
 const DrawerNavigator = createDrawerNavigator();
+
+const CustomDrawerContent = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+};
 
 const Drawer = () => {
   return (
     <DrawerNavigator.Navigator
       useLegacyImplementation={true}
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        headerShown: false,
+        drawerType: 'front',
+        drawerStyle: {
+          backgroundColor: '#F7FBF9',
+          width: '80%',
+        },
+      }}
     >
-      <DrawerNavigator.Screen name="Main" component={Main} />
+      <DrawerNavigator.Screen
+        name="Main"
+        component={Main}
+        options={{ drawerLabel: '홈' }}
+      />
       <DrawerNavigator.Screen name="ChangeName" component={ChangeName} />
+      <DrawerNavigator.Screen
+        name="PersonalInfoPolicy"
+        component={PersonalInfoPolicy}
+        options={{ drawerLabel: '개인정보처리방침' }}
+      />
     </DrawerNavigator.Navigator>
   );
 };
