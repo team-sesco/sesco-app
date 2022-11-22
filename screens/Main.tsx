6b95fdc83ac7b1010d04bc7893d5ad5471ff8bd7
Sangmin Lee <gink.ss00@gmail.com>
2022-11-23 04:20:39 +0900

    feat: 메인페이지 바탕색 구현

--- screens/Main.tsx
+++ screens/Main.tsx
@@ -6,6 +6,11 @@ import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
+const Background = styled.View`
+  width: 100%;
+  height: 100%;
+  background-color: #f7fbf9;
+`;
 
 const Container = styled.ScrollView`
   width: 95%;
@@ -97,35 +102,37 @@ const Main = () => {
   };
 
   return (
-    <Container>
-      <VSeparator />
-      <MainBannerBtn>
-        <MainBannerText>SE. SCO를 처음 이용하시나요?</MainBannerText>
-        <MainBannerText2>이용 방법</MainBannerText2>
-        <Ionicons name="chevron-forward" color="#98A1BD" size={24} />
-      </MainBannerBtn>
-      <VSeparator />
-      <DetectPestBtn onPress={goToDetectPest}>
-        <DetectPestInnerBorder>
-          <DetectPestText>병해충 탐지하기</DetectPestText>
-        </DetectPestInnerBorder>
-      </DetectPestBtn>
-      <VSeparator />
-      <NormalBtnWrapper>
-        <NormalBtn>
-          <Ionicons name="home-outline" color="#48a346" size={24} />
-          <NormalBtnText>내 농작물</NormalBtnText>
-        </NormalBtn>
-        <NormalBtn>
-          <Ionicons name="map-outline" color="#48a346" size={24} />
-          <NormalBtnText>지도</NormalBtnText>
-        </NormalBtn>
-      </NormalBtnWrapper>
-      <VSeparator />
-      <Title>즐겨찾는 나의 작물</Title>
-      <VSeparator />
-      <Title>최근 탐지 기록</Title>
-    </Container>
+    <Background>
+      <Container>
+        <VSeparator />
+        <MainBannerBtn>
+          <MainBannerText>SE. SCO를 처음 이용하시나요?</MainBannerText>
+          <MainBannerText2>이용 방법</MainBannerText2>
+          <Ionicons name="chevron-forward" color="#98A1BD" size={24} />
+        </MainBannerBtn>
+        <VSeparator />
+        <DetectPestBtn onPress={goToDetectPest}>
+          <DetectPestInnerBorder>
+            <DetectPestText>병해충 탐지하기</DetectPestText>
+          </DetectPestInnerBorder>
+        </DetectPestBtn>
+        <VSeparator />
+        <NormalBtnWrapper>
+          <NormalBtn>
+            <Ionicons name="home-outline" color="#48a346" size={24} />
+            <NormalBtnText>내 농작물</NormalBtnText>
+          </NormalBtn>
+          <NormalBtn>
+            <Ionicons name="map-outline" color="#48a346" size={24} />
+            <NormalBtnText>지도</NormalBtnText>
+          </NormalBtn>
+        </NormalBtnWrapper>
+        <VSeparator />
+        <Title>즐겨찾는 나의 작물</Title>
+        <VSeparator />
+        <Title>최근 탐지 기록</Title>
+      </Container>
+    </Background>
   );
 };
 
