af1d89459548ced71b521d03bf73912cde6e546c
Sangmin Lee <gink.ss00@gmail.com>
2022-11-24 17:41:31 +0900

    feat: 메인 - 지도 페이지 구현

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import { Ionicons } from '@expo/vector-icons';
+import MapView from 'react-native-maps';
+import { Dimensions, Platform, StatusBar } from 'react-native';
 
 const Map = () => {
-  return;
+  const { height: PHONE_HEIGHT } = Dimensions.get('window');
+  return (
+      <MapView
+        style={{ height: PHONE_HEIGHT }}
+        userInterfaceStyle="light"
+        showsUserLocation={true}
+        rotateEnabled={false}
+        pitchEnabled={false}
+        initialRegion={{
+          latitude: 37.587378,
+          longitude: 127.09763,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      ></MapView>
+  );
 };
 
 export default Map;
