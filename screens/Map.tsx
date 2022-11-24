2a1d093e60f8204a7576556f997053fec0c71616
Sangmin Lee <gink.ss00@gmail.com>
2022-11-24 17:41:54 +0900

    feat: 지도 페이지 뒤로가기 시 메인으로 이동 구현

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import MapView from 'react-native-maps';
 import { Dimensions, Platform, StatusBar } from 'react-native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
+const BackBotton = styled.TouchableOpacity<{ statusbarHeight: number }>`
+  position: absolute;
+  left: 10px;
+  top: ${(props) => props.statusbarHeight}px;
+  z-index: 100;
+  margin-left: 10px;
+  margin-bottom: 15px;
+  border: 2px solid #000;
+  border-radius: 25px;
+  background-color: #fff;
+`;
 const Map = () => {
   const { height: PHONE_HEIGHT } = Dimensions.get('window');
+  const STATUSBAR_HEIGHT =
+    Platform.OS === 'ios' ? getStatusBarHeight(true) : StatusBar.currentHeight;
+  const navigation = useNavigation();
+  const goToMain = () => {
+    //@ts-ignore
+    navigation.reset({ routes: [{ name: 'Drawer' }] });
+  };
   return (
+    <>
+      <BackBotton onPress={goToMain} statusbarHeight={STATUSBAR_HEIGHT}>
+        <Ionicons name="chevron-back-outline" size={40} color="#000" />
+      </BackBotton>
       <MapView
         style={{ height: PHONE_HEIGHT }}
         userInterfaceStyle="light"
@@ -20,6 +44,7 @@ const Map = () => {
           longitudeDelta: 0.01,
         }}
       ></MapView>
+    </>
   );
 };
 
