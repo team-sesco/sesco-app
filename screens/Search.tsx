e483b906405de383b1df0b75de90d50a7e663b9a
Sangmin Lee <gink.ss00@gmail.com>
2022-11-29 06:06:56 +0900

    feat: 메인 페이지 - 검색 페이지 api 연동 및 구현

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
 import HeadSeparator from '../components/HeadSeparator';
 import carrotGIF from '../assets/carrot.gif';
+import gochuImg from '../assets/gochu.png';
+import muImg from '../assets/mu.png';
+import baechuImg from '../assets/baechu.png';
+import kongImg from '../assets/kong.png';
+import paImg from '../assets/pa.png';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons, AntDesign } from '@expo/vector-icons';
-import { Keyboard, TouchableOpacity } from 'react-native';
+import { Alert, Keyboard, TouchableOpacity } from 'react-native';
+import { BASE_URI } from '../api/api';
+import BookMarkButton from '../components/BookMarkButton';
 
 const LoadingBackground = styled.View<{ isLoading: boolean }>`
   position: absolute;
@@ -81,16 +88,73 @@ const NoAnyResultText = styled.Text`
   color: rgba(9, 9, 9, 0.4);
 `;
 
-const Search = (route: { params: { jwtToken } }) => {
+const ListView = styled.FlatList``;
+
+const Search = ({
+  route: {
+    params: { jwtToken, userName },
+  },
+}) => {
   const navigation = useNavigation();
   const [isReady, setIsReady] = useState(true);
   const [searchValue, setSearchValue] = useState('');
   const [isAnyClick, setIsAnyClick] = useState(false);
+  const [searchResult, setSearchResult] = useState([]);
+  const [hasResult, setHasResult] = useState(true);
 
   const goBack = () => {
     //@ts-ignore
     navigation.reset({ routes: [{ name: 'Drawer' }] });
   };
+
+  const goToDetectResult = async (detectionId) => {
+    setIsReady(false);
+    const response = await fetch(`${BASE_URI}/api/v1/detection/${detectionId}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }).then((res) => res.json());
+
+    if (response.msg === 'success') {
+      navigation.navigate('AlreadyDetectPestResult', {
+        response,
+      });
+
+      setIsReady(true);
+      return;
+    }
+    setIsReady(true);
+    Alert.alert('잠시 후 다시 시도해주세요!');
+  };
+
+  const submitSearchValue = async () => {
+    setIsReady(false);
+    const response = await fetch(`${BASE_URI}/api/v1/search`, {
+      method: 'POST',
+      body: JSON.stringify({
+        search_str: searchValue,
+      }),
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => res.json());
+
+    if (response.msg === 'success') {
+      setSearchResult(response.result);
+      setIsAnyClick(true);
+
+      if (response.result.length === 0) {
+        setHasResult(false);
+      } else {
+        setHasResult(true);
+      }
+    }
+
+    setIsReady(true);
+  };
+
   return (
     <TouchableOpacity onPress={Keyboard.dismiss} activeOpacity={1}>
       <>
@@ -105,9 +169,10 @@ const Search = (route: { params: { jwtToken } }) => {
             </BackBotton>
             <SearchText
               maxlength={20}
-              placeholder="작물명, 병해충명, 지역(동)명"
+              placeholder="작물명, 병해충명, 지역명"
               value={searchValue}
               onChangeText={setSearchValue}
+              onSubmitEditing={submitSearchValue}
             />
             <Ionicons
               name="ios-close-circle-sharp"
@@ -119,6 +184,7 @@ const Search = (route: { params: { jwtToken } }) => {
                 display: searchValue ? 'flex' : 'none',
               }}
               onPress={() => {
+                setHasResult(true);
                 setSearchValue('');
               }}
             />
@@ -129,6 +195,48 @@ const Search = (route: { params: { jwtToken } }) => {
               <AntDesign name="search1" color="rgba(9,9,9,0.4)" size={50} />
               <NoAnyValueText>사용자들의 병해충 정보를 검색해보세요</NoAnyValueText>
             </NoAnyValueView>
+            <NoAnyResultView isAnyResult={hasResult}>
+              <Ionicons
+                name="ios-close-circle-outline"
+                size={50}
+                color="rgba(9,9,9,0.4)"
+              />
+              <NoAnyResultText>검색 결과가 없습니다</NoAnyResultText>
+            </NoAnyResultView>
+            {searchResult.length !== 0 ? (
+              <ListView
+                data={searchResult}
+                renderItem={({ item }) => (
+                  <BookMarkButton
+                    onPress={() => {
+                      goToDetectResult(item._id);
+                    }}
+                    cropImage={
+                      item.category === '고추'
+                        ? gochuImg
+                        : item.category === '무'
+                        ? muImg
+                        : item.category === '배추'
+                        ? baechuImg
+                        : item.category === '콩'
+                        ? kongImg
+                        : item.category === '파'
+                        ? paImg
+                        : carrotGIF
+                    }
+                    cropLocation={item.location.address_name}
+                    cropName={item.category}
+                    cropPest={
+                      item.model_result.name.includes('정상')
+                        ? '정상'
+                        : item.model_result.name
+                    }
+                  />
+                )}
+                keyExtractor={(_, index) => index.toString()}
+                showsVerticalScrollIndicator={false}
+              />
+            ) : null}
           </Container>
         </Background>
       </>
